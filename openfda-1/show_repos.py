a7875d8d30e53cd7d6c6879a7e65e3cad8497799
mDafonte <md.dafonte@gmail.com>
2018-04-06 16:37:14 +0200

    aba

--- openfda-1/show_repos.py
+++ openfda-1/show_repos.py
@@ -4,15 +4,17 @@ import json
 headers = {'User-Agent': 'http-client'}
 
 conn = http.client.HTTPSConnection("api.fda.gov")
-conn.request("GET", "/drug/label.json", None, headers)
+conn.request("GET", "/drug/label.json?limit=10", None, headers)
 r1 = conn.getresponse()
 print(r1.status, r1.reason)
 repos_raw = r1.read().decode("utf-8")
 conn.close()
-print(repos_raw)
 repos = json.loads(repos_raw)
-
 
 print("The id of the first repository is",repos['results'][0]['id'])
 print("The purpose of the first repository is",repos['results'][0]['purpose'])
 print("The manufacturer name of the first repository is",repos['results'][0]['openfda']['manufacturer_name'])
+
+aa=[0,1,2,3,4,5,6,7,8,9]
+for i in aa:
+    print("The id is", repos["results"][i]["id"])
