fd67cf6bc8e9d2ee643b42fa3b7c280776aec4be
mDafonte <md.dafonte@gmail.com>
2018-05-08 18:39:45 +0200

    terminado sin revisar

--- openfda-4/openfda4.py
+++ openfda-4/openfda4.py
@@ -2,7 +2,7 @@ import http.server
 import json
 import socketserver
 socketserver.TCPServer.allow_reuse_adress = True
-
+IP="localhost"
 PORT = 8000
 
 # HTTPRequestHandler class
@@ -14,7 +14,18 @@ class testHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
         if self.path == "/":
             with open("search.html","r")as f:
                 message=f.read()
-
+        elif "search" in self.path:
+            conn = http.client.HTTPSConnection("api.fda.gov")
+            drug=self.path.split("&")[0].split("=")[1]
+            limit=self.path.split("&")[1].split("=")[1]
+            url = "/drug/label.json?search=active_ingredient:" + drug + "&" + "limit=" + limit
+            conn.request("GET", url, None, headers)
+            r1 = conn.getresponse()
+            info_bruto = r1.read().decode("utf-8")
+            conn.close()
+            message = json.loads(info_bruto)
+            message = str(info_bruto)
+        self.wfile.write(bytes(message, "utf8"))
         self.send_header('Content-type','text/html')
         self.end_headers()
         self.wfile.write(bytes(message, "utf8"))
@@ -22,6 +33,6 @@ class testHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
 
 Handler = http.server.SimpleHTTPRequestHandler
 Handler = testHTTPRequestHandler
-httpd = socketserver.TCPServer(("", PORT), Handler)
+httpd = socketserver.TCPServer((IP, PORT), Handler)
 print("serving at port", PORT)
 httpd.serve_forever()
