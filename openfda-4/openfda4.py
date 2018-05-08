36f27d71879213b1ca11ade420be5c3e928ff3a2
mDafonte <md.dafonte@gmail.com>
2018-05-08 10:45:37 +0200

    openfda4 #1

--- openfda-4/openfda4.py
+++ openfda-4/openfda4.py
@@ -1,4 +1,3 @@
-
 import http.server
 import json
 import socketserver
@@ -9,21 +8,34 @@ PORT = 8000
 class testHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
     # GET
     def do_GET(self):
-        # Send respnse status code
+        headers = {'User-Agent': 'http-client'}
+        drugs_id=[]
+        message=""
+        conn = http.client.HTTPSConnection("api.fda.gov")
+        conn.request("GET", "/drug/label.json?limit=10", None, headers)
+        r1 = conn.getresponse()
+        print(r1.status, r1.reason)
+        drugs_raw = r1.read().decode("utf-8")
+        drugs=json.loads(drugs_raw)
+        conn.close()
+        for i in range(len(drugs['results'])):
+            drugs_id.append(drugs["results"][i]["id"])
+        for elem in drugs_id:
+            message=message +"<ol>"+elem+"</ol>"
         self.send_response(200)
-
-        # Send Headers
-        self.send_header("Content-type","text/html")
+        with open("drugs.html","w")as f:
+            f.write(message)
+        with open("drugs.html","r") as f:
+            patata=f.read()
+        self.send_response(200)
+        self.send_header('Content-type','text/html')
         self.end_headers()
-        with open("search.html","r") as f:
-            message= f.read()
-        # Write content as utf-8 data
+        message = patata
         self.wfile.write(bytes(message, "utf8"))
         return
 
 Handler = http.server.SimpleHTTPRequestHandler
 Handler = testHTTPRequestHandler
-
 httpd = socketserver.TCPServer(("", PORT), Handler)
 print("serving at port", PORT)
 httpd.serve_forever()
